9b76f534f4a0bf8a16c3130bd3578ace2ecb29f3
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-08-08 16:46:57 +0200

    Add Get for all navpoints in EducationalMaterialNavpointsController.cs

--- EducationalAPI/Controllers/EducationalMaterialNavpointsController.cs
+++ EducationalAPI/Controllers/EducationalMaterialNavpointsController.cs
@@ -1,6 +1,34 @@
-﻿namespace EducationalAPI.Controllers
+﻿using EducationalAPI.Data.DAL;
+using EducationalAPI.Data.Models;
+
+namespace EducationalAPI.Controllers
 {
+    //[Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin,User")]
+    [Route("api/[controller]")]
+    [ApiController]
     public class EducationalMaterialNavpointsController : ControllerBase
     {
+        private readonly IMapper _mapper;
+        private readonly IGenericRepository<EduMatNavpoint> _eduMatNavpointRepository;
+        public EducationalMaterialNavpointsController(IMapper mapper, IGenericRepository<EduMatNavpoint> eduMatNavpointRepository)
+        {
+            _mapper = mapper;
+            _eduMatNavpointRepository = eduMatNavpointRepository;
+        }
+        /// <summary>
+        /// Use to receive all Educational Material Navpoints
+        /// </summary>
+        /// <returns>Navpoints</returns>
+        /// <response code="200">Ok</response>
+        /// <response code="404">Not found</response>
+        // GET: api/<EducationalAPI>/educationalmaterialnavpoints
+        [HttpGet("educationalmaterialnavpoints")]
+        //[Authorize(Roles = "Admin,User")]
+        public async Task<IActionResult> Get()
+        {
+            var eduMatNavpoints = _mapper.Map<IEnumerable<EduMatTypeReadDTO>>(await _eduMatNavpointRepository.GetAllAsync(Array.Empty<string>()));
+            if (eduMatNavpoints is null) return NotFound();
+            return Ok(eduMatNavpoints);
+        }
     }
 }
