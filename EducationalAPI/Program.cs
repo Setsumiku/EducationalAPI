b96588959ebfb203a2617444e805a86442f9d67d
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-08-08 14:10:31 +0200

    Add required nuget for automapper Add automapper to appbuilder in program.cs Move builder setup code to Setups method

--- EducationalAPI/Program.cs
+++ EducationalAPI/Program.cs
@@ -3,26 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("EducationalAPIDb");
-builder.Services.AddDbContext<EducationalAPIContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 Setups(builder);
-
-void Setups(WebApplicationBuilder builder)
-{
-    var logger = new LoggerConfiguration().WriteTo.File(builder.Configuration.GetValue<string>("Logging:FilePath")).CreateLogger();
-    builder.Logging.ClearProviders();
-    builder.Logging.AddSerilog(logger);
-}
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -36,3 +18,16 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void Setups(WebApplicationBuilder builder)
+{
+    var connectionString = builder.Configuration.GetConnectionString("EducationalAPIDb");
+    builder.Services.AddDbContext<EducationalAPIContext>(options => options.UseSqlServer(connectionString));
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    var logger = new LoggerConfiguration().WriteTo.File(builder.Configuration.GetValue<string>("Logging:FilePath")).CreateLogger();
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog(logger);
+}
