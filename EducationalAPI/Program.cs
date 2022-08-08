e2c3672bc40ff9e8bafefb0122cc0cfc90d5aeb8
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-08-08 13:33:52 +0200

    Import serilog nugets Add Setups method in program.cs Implement global logging to file

--- EducationalAPI/Program.cs
+++ EducationalAPI/Program.cs
@@ -1,5 +1,6 @@
 using EducationalAPI.Data.Context;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+Setups(builder);
+
+void Setups(WebApplicationBuilder builder)
+{
+    var logger = new LoggerConfiguration().WriteTo.File(builder.Configuration.GetValue<string>("Logging:FilePath")).CreateLogger();
+    builder.Logging.ClearProviders();
+    builder.Logging.AddSerilog(logger);
+}
 
 var app = builder.Build();
 
