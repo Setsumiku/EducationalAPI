76a2ea6b92cdab1acd7484a21c33f81f73b68b09
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-08-09 09:46:06 +0200

    implement UserLoginDTO

---
+++ EducationalAPI/DTO/UserLoginDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace EducationalAPI.DTO
+{
+    public class UserLoginDTO
+    {
+        internal int UserId { get; set; }
+        [Required]
+        public string UserLogin { get; set; }
+        [Required]
+        public string UserPassword { get; set; }
+        internal string UserRole { get; set; }
+    }
+}
